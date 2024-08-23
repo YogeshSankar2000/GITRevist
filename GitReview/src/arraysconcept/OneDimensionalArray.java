432012cb6bc38c08e775b4818d87460e00ade283
Yogi <yogeshshankarkala@gmail.com>
2024-08-23 23:00:22 +0530

    Third Commit

--- GitReview/src/arraysconcept/OneDimensionalArray.java
+++ GitReview/src/arraysconcept/OneDimensionalArray.java
@@ -11,12 +11,10 @@ public class OneDimensionalArray {
 //		int[] a = {1,2,3,4,5};
 		for(int i=0;i<=a.length-1;i++) {
 			System.out.println(a[i]);
-		}
+		}  
 		
 		
 		
-		for(int b:a) {
-			System.out.println(b);
-		}
+	
 	}
 }
